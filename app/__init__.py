e41c413e14451be34793f8bf4623f7c56a8b2db4
Rafael Catucuamba <jcatucuambau@est.ups.edu.ec>
2026-07-06 09:11:58 -0500

    crear modelos y datos de prueba

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,12 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Modelos
-    from app.models import Usuario
+    from app.models import Usuario, Categoria, Producto, Pedido, DetallePedido
 
     # Blueprints
     from app.blueprints.public import public_bp
@@ -22,7 +22,7 @@ def create_app():
     from app.blueprints.admin import admin_bp
 
     app.register_blueprint(public_bp)
-    app.register_blueprint(auth_bp, url_prefix='/auth')
-    app.register_blueprint(admin_bp, url_prefix='/admin')
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(admin_bp)
 
     return app
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,4 @@
 from app.models.usuario import Usuario
+from app.models.categoria import Categoria
+from app.models.producto import Producto
+from app.models.pedido import Pedido, DetallePedido
